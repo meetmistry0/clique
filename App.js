82cd6f26a8e7829971a0c98e1f5012729d40f872
meetmistry0 <meet@outlook.com>
2022-07-12 19:45:42 +0530

    Update `DataScreen.js` & `App.js`

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
-import DataScreen from './screens/DataScreen'
+import DataScreen from './screens/DataScreen';
 
 const Stack = createNativeStackNavigator();
 
--- screens/DataScreen.js
+++ screens/DataScreen.js
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
 
 const DataScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
+            <StatusBar style="auto" />
             <View>
                 <Text>DataScreen</Text>
             </View>
