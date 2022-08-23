9b89397263b0b42a3835ccce65e6c7deb5476067
meetmistry0 <meet@outlook.com>
2022-08-24 00:06:06 +0530

    Add `useFocusEffect` & make item-id a variable

--- screens/DataScreen.js
+++ screens/DataScreen.js
@@ -1,17 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { useRoute } from '@react-navigation/core';
+import { useFocusEffect } from '@react-navigation/native';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import React from 'react';
 
 const DataScreen = () => {
     const route = useRoute();
+    let itemID = route.params.itemId
+
+    useFocusEffect(
+        React.useCallback(() => {
+            alert(itemID);
+            return () => {
+                // alert('Screen was unfocused');
+                // Do something when the screen is unfocused
+                // Useful for cleanup functions
+            };
+        }, [])
+    );
+
 
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar style="auto" />
             <View>
                 <Text>DataScreen</Text>
-                <Text>{route.params.itemId}</Text>
+                <Text>{itemID}</Text>
             </View>
         </SafeAreaView>
     )
