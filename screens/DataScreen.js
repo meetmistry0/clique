e7596c80faa4dd3b9ec5ae9acea2e41736e8b765
meetmistry0 <meet@outlook.com>
2022-08-30 18:52:47 +0530

    data screen reference

--- screens/DataScreen.js
+++ screens/DataScreen.js
@@ -1,19 +1,35 @@
+import React from 'react';
 import { StatusBar } from "expo-status-bar";
 import { useRoute } from '@react-navigation/core';
 import { useFocusEffect } from '@react-navigation/native';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import React from 'react';
+import {
+    StyleSheet, Text, View, SafeAreaView, TouchableOpacity,
+} from 'react-native';
+import { collection, doc, getDocs, getDoc } from 'firebase/firestore/lite'
+import { db } from '../firebase/firebase-config';
 
 const DataScreen = () => {
     const route = useRoute();
     let itemID = route.params.itemId
 
+    const ItemData = async () => {
+
+        const itemRef = doc(db, "item_master", `${route.params.itemId}`);
+        const itemData = await getDoc(itemRef);
+
+        if (itemData.exists()) {
+            // console.log("Document data:", itemData.data());
+            ToastAndroid.show("data found", ToastAndroid.SHORT);
+        } else {
+            ToastAndroid.show("No such product!", ToastAndroid.SHORT);
+        }
+    }
+
     useFocusEffect(
         React.useCallback(() => {
             alert(itemID);
             return () => {
-                // alert('Screen was unfocused');
-                // Do something when the screen is unfocused
+                alert('Screen was unfocused');
                 // Useful for cleanup functions
             };
         }, [])
@@ -26,6 +42,12 @@ const DataScreen = () => {
             <View>
                 <Text>DataScreen</Text>
                 <Text>{itemID}</Text>
+                <TouchableOpacity
+                    onPress={ItemData}
+                    style={styles.searchButton}
+                >
+                    <Text style={styles.searchButtonText}>get data</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
@@ -36,5 +58,21 @@ export default DataScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+
+    searchButton: {
+        marginTop: 12,
+        marginBottom: 12,
+        backgroundColor: '#000000',
+        width: '24%',
+        borderRadius: 12,
+        height: 48,
+        alignItems: 'center',
+        justifyContent: "center",
+    },
+
+    searchButtonText: {
+        color: 'white',
+        fontWeight: '600',
     },
 })
