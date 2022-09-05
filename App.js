bc856b79a121d97df49fd686f1c613108b0f3156
meetmistry0 <meet@outlook.com>
2022-09-05 13:38:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
+// import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import DataScreen from './screens/DataScreen';
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        {/* <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} /> */}
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Data" component={DataScreen} />
       </Stack.Navigator>
